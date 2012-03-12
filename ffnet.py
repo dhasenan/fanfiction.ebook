3844f515b29eb67d39829c7f94caeb171eb5c5d6
Christopher Wright <gareis@ps86435.dreamhostps.com>
2012-03-12 15:22:58 -0700

    Made ebook generation DRY for rationality and others
    There's a fledgling API that I should document. Need to add command line
    options eventually.

--- ffnet.py
+++ ffnet.py
@@ -9,123 +9,129 @@ import sys
 
 TARGET="http://www.fanfiction.net/s/%s/%s/"
 
-def find_count(c):
-	count = 0
-	a, b, kernel = c.partition("SELECT title='chapter navigation'")
-	kernel, a, b = kernel.partition("</select>")
-	f = re.compile("option\\s*value=([0-9]*)")
-	for match in f.finditer(kernel):
-		g, = match.groups(1)
-		current = int(g)
-		if count < current:
-			count = current
-	print "total of %s chapters" % count
-	return count
+class FFNetMunger:
+    def __init__(self, story_id):
+        self.story_id = story_id
+        self.div_re = re.compile('<div.*?</div>', re.DOTALL)
+
+    def process(self):
+        self.content, self.name = self.retrieve()
+        if self.filename is None or self.filename is "":
+            self.filename = self.name
+        self.write()
+        self.convert()
+
+    def find_count(self, c):
+        count = 0
+        a, b, kernel = c.partition("SELECT title='chapter navigation'")
+        kernel, a, b = kernel.partition("</select>")
+        f = re.compile("option\\s*value=([0-9]*)")
+        for match in f.finditer(kernel):
+            g, = match.groups(1)
+            current = int(g)
+            if count < current:
+                count = current
+        print "total of %s chapters" % count
+        return count
 
 
-def guts(c):
-	kernel = re.split("<div[^>]*class=storytext\\b.*?>", c, 1)[1]
-	while True:
-		kernel, n = re.subn('<div.*?</div>', '', kernel, flags=re.DOTALL)
-		if n == 0:
-			break
-	kernel, a, b = kernel.partition("</div>")
-	return kernel
+    def guts(self, c):
+        kernel = re.split("<div[^>]*class=storytext\\b.*?>", c, 1)[1]
+        while True:
+            kernel, n = self.div_re.subn('', kernel)
+            if n == 0:
+                break
+        kernel, a, b = kernel.partition("</div>")
+        return kernel
 
-def get_name(chapter):
-	a, b, kernel = chapter.partition("<title>")
-	kernel, a, b = kernel.partition("Chapter")
-	return kernel.strip()
+    def get_name(self, chapter):
+        a, b, kernel = chapter.partition("<title>")
+        kernel, a, b = kernel.partition("Chapter")
+        return kernel.strip()
 
-def title(c):
-	a, b, kernel = c.partition("<title>")
-	kernel, a, b = kernel.partition("</title>")
-	a, b, kernel = kernel.partition("Rationality ")
-	kernel, a, b = kernel.partition(", a Harry Potter")
-	return kernel
+    def title(self, c):
+        a, b, kernel = c.partition("<title>")
+        kernel, a, b = kernel.partition("</title>")
+        a, b, kernel = kernel.partition("Chapter ")
+        kernel, a, b = kernel.rpartition(",")
+        return "Chapter " + kernel # calibre needs 'chapter' to help its chapter detection
 
-def download(id, chapter):
-	print "retrieving chapter %s" % chapter
-	return urllib2.urlopen(TARGET % (id, chapter)).read()
-#	b = StringIO()
-#	c = Curl()
-#	c.setopt(URL, TARGET % (id, chapter))
-#	c.setopt(WRITEFUNCTION, b.write)
-#	c.perform()
-#	c.close()
-#	return b.getvalue()
+    def download(self, id, chapter):
+        print "retrieving chapter %s" % chapter
+        # TODO this doesn't like non-ascii characters
+        return urllib2.urlopen(TARGET % (id, chapter)).read()
 
-def retrieve(id):
-	chapters = []
-	titles = []
+    def retrieve(self):
+        chapters = []
+        titles = []
 
-	first = download(id, 1)
-	count = find_count(first)
-	name = get_name(first)
-	chapters.append(first)
-	titles.append('')
+        first = self.download(self.story_id, 1)
+        count = self.find_count(first)
+        name = self.get_name(first)
+        chapters.append(first)
+        titles.append('')
 
-	# grab the items:
-	for i in range(2, count + 1):
-		chapters.append(download(id, i))
-		titles.append('')
+        # grab the items:
+        for i in range(2, count + 1):
+            chapters.append(self.download(self.story_id, i))
+            titles.append('')
 
 
-	# extract the essential bits from each chapter:
-	for i in range(len(chapters)):
-		print "munging chapter %s" % (i + 1)
-		c = chapters[i]
-		chapters[i] = guts(c)
-		titles[i] = title(c)
+        # extract the essential bits from each chapter:
+        for i in range(len(chapters)):
+            print "munging chapter %s" % (i + 1)
+            c = chapters[i]
+            chapters[i] = self.guts(c)
+            titles[i] = self.title(c)
 
-	# jam it into one string, with appropriate header and footer:
-	contents = StringIO()
-	contents.write("""<html>
-	<head>
-		<title>%s</title>
-	</head>
-	<body>
-""" % name)
-	for i in range(len(chapters)):
-		contents.write("<h1>" + titles[i] + "</h1>\n<div>" + chapters[i] + "</div>\n")
+        # jam it into one string, with appropriate header and footer:
+        contents = StringIO()
+        contents.write("""<html>
+        <head>
+            <title>%s</title>
+        </head>
+        <body>
+    """ % name)
+        for i in range(len(chapters)):
+            contents.write("<h1>" + titles[i] + "</h1>\n<div>" + chapters[i] + "</div>\n")
 
-	contents.write("</body></html>")
+        contents.write("</body></html>")
 
-	return contents, name
+        return contents, name
 
-def write(contents, name):
-	f = io.open(name + ".html", "wb")
-	f.write(contents.getvalue())
-	f.flush()
-	f.close()
-	return name
+    def write(self):
+        f = io.open(self.filename + ".html", "wb")
+        f.write(self.content.getvalue())
+        f.flush()
+        f.close()
 
-def convert(name):
-	pid1 = os.fork()
-	if pid1 == 0:
-		os.execlp("ebook-convert", "/usr/bin/ebook-convert", name + ".html", ".epub")
+    def args(self, outtype):
+        std_args = ["", self.filename + ".html", outtype]
 
-	pid2 = os.fork()
-	if pid2 == 0:
-		os.execlp("ebook-convert", "/usr/bin/ebook-convert", name + ".html", ".mobi")
+        if self.author != None:
+            std_args += ["--authors", self.author]
 
-	os.waitpid(pid1, 0)
-	os.waitpid(pid2, 0)
+        if self.cover != None:
+            std_args += ["--cover", self.cover]
+        return std_args
 
-def post():
-	pass
-#    print "posting mobi..."
-#    os.execlp("scp", "rationality.mobi", "ikeran.org:ikeran.org/")
-#    print "posting epub..."
-#    os.execlp("scp", "rationality.epub", "ikeran.org:ikeran.org/")
-#    print "done"
+    def convert(self):
+        # Prioritize low resource usage over speediness
+        pid1 = os.fork()
+        if pid1 == 0:
+            os.execvp("ebook-convert", self.args(".epub"))
+            return
+        os.waitpid(pid1, 0)
 
-if len(sys.argv) == 1:
-	print "Usage: %s STORY_ID"
-	exit(1)
+        pid2 = os.fork()
+        if pid2 == 0:
+            os.execvp("ebook-convert", self.args(".mobi"))
+            return
+        os.waitpid(pid2, 0)
 
-
-content, name = retrieve(sys.argv[1])
-write(content, name)
-convert(name)
-post()
+if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        print "Usage: %s STORY_ID"
+        exit(1)
+    munger = FFNetMunger(sys.argv[1])
+    munger.process()
